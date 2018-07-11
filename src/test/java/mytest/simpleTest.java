1b80fe0f8a61364f964550027bcfb7c7c8ef493c
daiwei <18428385839@163.com>
2018-07-11 11:36:57 +0800

    Test

---
+++ src/test/java/mytest/simpleTest.java
@@ -0,0 +1,15 @@
+package mytest;
+
+import org.junit.Test;
+
+/**
+ * @author daiwei
+ * @date 2018/7/11 11:34
+ */
+public class simpleTest {
+
+    @Test
+    public void test1(){
+        System.out.println("hello world");
+    }
+}
